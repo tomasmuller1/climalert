f64d1feff02edeee5df8b21a36495a4bfa11db5e
tomasmuller1 <tmuller@frba.utn.edu.ar>
2026-07-04 16:40:35 -0300

    Estructura inicial generada con Spring Initializr

---
+++ src/main/java/ar/edu/utn/frba/ddsi/climalert/ClimalertApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.ddsi.climalert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimalertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimalertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/frba/ddsi/climalert/ClimalertApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.ddsi.climalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
